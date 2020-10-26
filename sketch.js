9c891258960b92f3ce8326667497fb7ba98450cd
trugamr <6trugamr9@gmail.com>
2020-10-26 18:32:17 +0530

    emoji art

--- sketch.js
+++ sketch.js
@@ -8,19 +8,20 @@ const settings = {
 }
 
 const sketch = () => {
-  const colorCount = random.rangeFloor(2, 4)
+  const colorCount = random.rangeFloor(1, 5)
   const palette = random.shuffle(random.pick(palettes)).slice(colorCount)
 
   const createGrid = () => {
     const points = []
-    const count = 22
+    const count = 100
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
+        const radius = Math.abs(random.noise2D(u, v) * 0.1)
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(random.gaussian() * 0.03),
+          radius,
           position: [u, v],
         })
       }
@@ -36,20 +37,33 @@ const sketch = () => {
     width,
     height,
   }) => {
-    context.fillStyle = random.pick(palette)
+    context.fillStyle = 'white'
     context.fillRect(0, 0, width, height)
 
+    context.arc(width / 2, height / 2, 400, 0, Math.PI * 2)
+    context.fillStyle = 'black'
+    context.fill()
+
     points
-      .filter(() => random.rangeFloor(0, 2))
+      // .filter(() => random.rangeFloor(0, 2))
       .forEach(({ color, radius, position: [u, v] }) => {
         const x = lerp(margin, width - margin, u)
         const y = lerp(margin, height - margin, v)
 
-        context.beginPath()
-        context.arc(x, y, radius * width, 0, Math.PI * 2)
-        context.fillStyle = color
+        // context.beginPath()
+        // context.arc(x, y, radius * width, 0, Math.PI * 2)
+        // context.fillStyle = color
+        // context.fill()
+
+        context.fillStyle = random.rangeFloor(0, 2) ? 'pink' : 'black'
+        context.fillText('.', x, y)
+        context.font = `${radius * width}px "Fira Code"`
         context.fill()
       })
+
+    context.fillStyle = 'black'
+    context.font = `600px "Fira Code"`
+    context.fillText('🌸', width / 2 - 400, height / 2 + 190)
   }
 }
 
